21554a9f60596407b81e750a114988f1fa760eeb
jgunn <j.s.gunn22@gmail.com>
2023-11-15 11:47:43 -0800

    change app name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
-const appName = "smell-o-scope";
-
 mongoose.connect(
-  process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/${appName}`
+  process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/smell-o-scope`
 );
 
 module.exports = mongoose.connection;
