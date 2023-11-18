d9331d0955b271f395053ff0f0e5c4c4e54f97dd
Luis00809 <davidcarvajal008@gmail.com>
2023-11-18 10:34:05 -0800

    mutation for creating an item and adding to community

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -82,8 +82,6 @@ const resolvers = {
         community.users.push(userID);
         await community.save();
       }
-
-
       return Community.findOne({ _id: communityId }).populate("users");
     },
     sendMessage: async (_, { sender, recipient, content }, context) => {
