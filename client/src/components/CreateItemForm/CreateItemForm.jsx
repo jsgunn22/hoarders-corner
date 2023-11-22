e74df62c24c6bc2538890573ab4a4ed8807ade0f
Peyton Mastropolo <135174286+pmastropolo@users.noreply.github.com>
2023-11-22 08:36:04 -0600

    Update CreateItemForm.jsx

--- client/src/components/CreateItemForm/CreateItemForm.jsx
+++ client/src/components/CreateItemForm/CreateItemForm.jsx
@@ -4,7 +4,7 @@ import TextArea from "../Atoms/TextArea";
 import Checkbox from "../Atoms/Checkbox";
 import { useState } from "react";
 import { useMutation } from "@apollo/client";
-import { ADD_ITEM } from "../../utils/mutations";
+import { ADD_ITEM, UPLOAD_IMAGE } from "../../utils/mutations";
 import Auth from "../../utils/auth";
 import { QUERY_COMMUNITY_ITEMS } from "../../utils/queries";
 
@@ -16,7 +16,9 @@ export default function CreateItemForm({
   const [createItem, { error, data }] = useMutation(ADD_ITEM, {
     refetchQueries: [QUERY_COMMUNITY_ITEMS, "communities"],
   });
+  const [uploadImage] = useMutation(UPLOAD_IMAGE);
   const [isPublic, setIsPublic] = useState(false);
+  const [image, setImage] = useState(null);
   const [formState, setFormState] = useState({
     name: "",
     description: "",
@@ -37,6 +39,23 @@ export default function CreateItemForm({
     setFormState({ ...formState, isPublic: !isPublic });
   };
 
+  const handleImageChange = async (event) => {
+    const file = event.target.files[0];
+    setImage(file);
+
+    try {
+      const response = await uploadImage({
+        variables: {
+          file,
+        },
+      });
+      console.log('Uploaded image:', response.data);
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
+};
+
+  
   const createNewItem = async () => {
     console.log("test");
     event.preventDefault();
@@ -49,6 +68,7 @@ export default function CreateItemForm({
           owner: Auth.getProfile().authenticatedPerson.username,
           community: communityName,
           communityId: communityId,
+          image,
         },
       });
     } catch (error) {
@@ -90,6 +110,16 @@ export default function CreateItemForm({
               checked={isPublic}
               name={"isPublic"}
             />
+            
+<label htmlFor="imageUpload" className="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">
+Upload an image: 
+    <input id="imageUpload" 
+    type="file" 
+    accept=".jpg, .jpeg, .png, .gif" 
+    onChange={handleImageChange} 
+    />
+</label>
+            
           </>
         }
       />
