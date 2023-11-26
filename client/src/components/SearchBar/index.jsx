feae37f2fedea0acf3c469b70738b7bb6d2a928c
Jeffrey Gunn <j.s.gunn22@gmail.com>
2023-11-26 08:22:21 -0800

    fixes auth error when not logged in.  Was crashing the app

--- client/src/components/SearchBar/index.jsx
+++ client/src/components/SearchBar/index.jsx
@@ -1,24 +1,20 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Button from "../Atoms/Button";
+import Input from "../Atoms/Input";
 
-// When implementing the prop "body" it needs to be an input element to 
+// When implementing the prop "body" it needs to be an input element to
 // to able to use a useState on our search
 // can reference the call of modal in AllCommunities page
 
-export default function SearchBar({
-    btnAction,
-    bType,
-    body
-
-}) {
-    return (
-        <div className='flex justify-center mb-4 '>
-            <div className="mr-1 " >
-                {body}
-            </div>
-            <div>
-                <Button  label="Search" action={btnAction} type={bType} />
-            </div>
-        </div>
-    )
+export default function SearchBar({ btnAction, bType, body }) {
+  return (
+    <div className="flex justify-center mb-4 ">
+      <div className="mr-1 ">
+        {body} <Input label="Find a Community" />
+      </div>
+      <div>
+        <Button label="Search" action={btnAction} type={bType} />
+      </div>
+    </div>
+  );
 }
