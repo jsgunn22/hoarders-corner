5cdb4e88b473fa37e6b11c11bbe0af1e0357e303
Peyton Mastropolo <amyhytwn@aol.com>
2023-11-27 22:50:40 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const path = require("path");
+const multer = require("multer"); 
 
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
